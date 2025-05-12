4419f11e35fa76af920ac0cd767ea40fa3f131aa
Quetz-Lab <quetz.yggdrasil24@outlook.com>
2025-05-12 15:00:04 -0600

    Namespace

--- include/GameObject.h
+++ include/GameObject.h
@@ -1,22 +1,26 @@
 #pragma once
 #include "raylib.h"
 #include "string"
-using namespace std;
-class GameObject
+namespace AQ_QuetzLab
 {
+	using namespace std;
+	class GameObject
+	{
 
-public:
-	Vector2 position;
-	string name;
+	public:
+		Vector2 position;
+		string name;
 
-	Texture texture;
-	bool displayName = false;
+		Texture texture;
+		bool displayName = false;
 
-	GameObject( Vector2 pos, string _name, Texture tex  ):
-		position(pos), name(_name), texture(tex) { }
+		GameObject(Vector2 pos, string _name, Texture tex) :
+			position(pos), name(_name), texture(tex) {
+		}
 
-	virtual void update();
-	void draw();
+		virtual void update();
+		void draw();
 
-};
+	};
+}
 
